7630f75de2c6edb4c5616ee4c563630a8e288846
timoshimmo <tokmangwang@gmail.com>
2026-03-09 15:16:15 +0100

    Sort Out Tenant Admin Number 2

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,6 +2,7 @@ import { Module, Global } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Tenant, TenantSchema } from './schemas/tenant.schema';
 import { Order, OrderSchema } from './schemas/order.schema';
+import { Product, ProductSchema } from './schemas/product.schema';
 import { DbService } from './db.service';
 
 @Global()
@@ -10,6 +11,7 @@ import { DbService } from './db.service';
     MongooseModule.forFeature([
       { name: Tenant.name, schema: TenantSchema },
       { name: Order.name, schema: OrderSchema },
+      { name: Product.name, schema: ProductSchema },
     ]),
   ],
   providers: [DbService],
